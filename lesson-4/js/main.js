1721d9831ef4fbb5facb2fb246922121d8624ebb
Josh Bailey <joshuaclaytonbailey@gmail.com>
2020-02-11 23:17:37 -0800

    Added javascript functionality

--- lesson-4/js/main.js
+++ lesson-4/js/main.js
@@ -1,9 +1,24 @@
+document.getElementsByTagName("a")[0].setAttribute('title', "github");
 
-/*
-* JAVASCRIPT FUNCTION
-* @parameter: This does ____
-*
-* Notes: Lorem ipsum justo vel
-* */
+let list = document.getElementsByTagName("ul")[0];
+list.getElementsByTagName("li")[1].setAttribute("class", "strengths");
+
+document.getElementById("text-modify").innerHTML = "Check out this cat picture!"
+
+let image = document.getElementById("catImage")
+let hideButton = document.getElementById("hideImage")
+
+function hideImage() {
+    if (image.style.display === "none") {
+        image.style.display = "block";
+        hideButton.innerHTML = "Hide Image"
+    }else {
+        image.style.display = "none";
+        hideButton.innerHTML = "Show Image"
+    }
+} 
+function thankMessage() {
+    alert("Thank you for viewing my resume!")
+}
 
 console.log("This outputs into the console.");
