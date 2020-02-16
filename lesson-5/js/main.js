93584b5eda34d16269c95ea09a74bc1bb90371fd
Josh Bailey <joshuaclaytonbailey@gmail.com>
2020-02-16 03:03:31 -0800

    built login page

--- lesson-5/js/main.js
+++ lesson-5/js/main.js
@@ -6,4 +6,8 @@
 * Notes: Lorem ipsum justo vel
 * */
 
+function loginFail() {
+    alert('We are unable to sign you in at this time. Please come back later.');
+}
+
 console.log("This outputs into the console.");
